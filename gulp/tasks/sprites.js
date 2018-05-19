a041098d01b0f9479d93416bdd02ec3eb1256199
Sangwoo Park <eicpein@gmail.com>
2018-05-19 15:39:56 +0900

    Remove previously generated sprite and CSS

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,5 +1,6 @@
 import gulp from 'gulp'
 import svgSprite from 'gulp-svg-sprite'
+import del from 'del'
 
 const config = {
 	mode: {
@@ -14,8 +15,14 @@ const config = {
 	},
 }
 
-gulp.task( 'sprites', function() {
+gulp.task( 'clean', function() {
+	return del( './app/temp/sprite' )
+} )
+
+gulp.task( 'createSprite', function() {
 	return gulp.src( './app/assets/images/icons/**/*.svg' )
 		.pipe( svgSprite( config ) )
 		.pipe( gulp.dest( './app/temp/sprite' ) )
 } )
+
+gulp.task( 'sprites', gulp.series( 'clean', 'createSprite' ) )
