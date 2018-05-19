9adf61f7e3c7a97261811c91aae7f3be32e52df5
Sangwoo Park <eicpein@gmail.com>
2018-05-19 13:49:30 +0900

    Reveal feature-items when the user scrolls down

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -7,9 +7,24 @@ export default class RevealOnScroll {
 		this.offsetPercentage = offset
 
 		this.hideInitially()
+		this.createWaypoints()
 	}
 
 	hideInitially() {
 		this.itemsToReveal.addClass( "reveal-item" )
+	}
+
+	createWaypoints() {
+		const thisClass = this;
+		this.itemsToReveal.each( function() {
+			var currentItem = this;
+			new Waypoint( {
+				element: currentItem,
+				handler: function() {
+					$( currentItem ).addClass( "reveal-item--is-visible" )
+				},
+				offset: thisClass.offsetPercentage,
+			} )
+		} )
 	}
 }
